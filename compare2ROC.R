8a5435626ca2c908a38e45cb6c3f38ba1708f08e
John Pluta <jpluta26@gmail.com>
2019-07-19 13:19:46 -0400

    Update compare2ROC.R

--- compare2ROC.R
+++ compare2ROC.R
@@ -20,12 +20,12 @@ compare2ROC <- function( roc.dat1, roc.dat2, title, grp1.name, grp2.name)
   # probably should change this to a ggplot object and pass, rather than write the plot in the function
   # summary plot of the test
   png(paste(title, "png", sep = "."))
-  text.tcga <- paste(grp1.name, " AUC = ", round(roc.dat1[[2]][[1]][1], 2), sep = "")
-  text.pen <- paste(grp2.name, " AUC = ", round( roc.dat2[[2]][[1]][1], 2), sep = "")
+  text1 <- paste(grp1.name, " AUC = ", round(roc.dat1[[2]][[1]][1], 2), sep = "")
+  text2 <- paste(grp2.name, " AUC = ", round( roc.dat2[[2]][[1]][1], 2), sep = "")
   plot(roc.dat1[[1]], col = "blue", main = title)
   plot(roc.dat2[[1]], col = "red", add = TRUE)
   mtext(p.annot, side = 3)
-  legend(x = 0.5, y = 0.2, cex = .9, legend = c(text.tcga, text.pen), col = c("blue", "red"), lwd = 3)
+  legend(x = 0.5, y = 0.2, cex = .9, legend = c(grp1.name, grp2.name), col = c("blue", "red"), lwd = 3)
   dev.off()
 }  
 # -------------------------------------------------------------------------- #
