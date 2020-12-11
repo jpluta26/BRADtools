cd0c35c76f73a7dac6fe98c1cf96e582ca4f5508
John Pluta <jpluta26@gmail.com>
2020-12-11 15:10:15 -0500

    Update mapVarToRsID.R

--- mapVarToRsID.R
+++ mapVarToRsID.R
@@ -13,11 +13,13 @@ if(length(args) < 2)
   print('need to provide arguments: ')
   print('SNPLST, a text file of snps in chr:pos format')
   print('OUTFILE, a string of the output file name')
+  print('BUILD, one of "grch37" or "grch38"')
   stop()
 }
 
 INFILE  = args[1]
 OUTFILE = args[2]
+BUILD = args[3]
 dat <- as.data.frame(read.table(INFILE, header = F))
 
 dat$MarkerName <- as.character(dat$V1)
@@ -26,10 +28,15 @@ dat$BP <- unlist(lapply(strsplit(dat$MarkerName, ":"), function(x) x[2]))
 
 out <- c()
 			
+if(!(BUILD %in% c("grch37", "grch38"))
+{
+	   stop("invalid option for build")
+}
+			
 for( i in unique(dat$CHR))
 {
   # change this line if running else where
-	REFFILE <- paste("/project/knathanslab/REF/HHRC/HRC-chrbp-rsid-map-chr", i, ".txt", sep = "")
+	REFFILE <- paste("/project/knathanslab/REF/HHRC/", BUILD, "HRC-chrbp-rsid-map-chr", i, ".txt", sep = "")
 	ref <- as.data.frame(fread(REFFILE, header = F))
 
 	tmp <- dat[ dat$CHR == i,]
