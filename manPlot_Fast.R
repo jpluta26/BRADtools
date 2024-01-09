440a521e6641a32ccf95b6f71de5ffdea55a6b54
John Pluta <jpluta26@gmail.com>
2024-01-09 14:03:02 -0500

    Update manPlot_Fast.R
    added qq plotting

--- manPlot_Fast.R
+++ manPlot_Fast.R
@@ -1,6 +1,6 @@
 #!/usr/bin/env Rscript
 # pluta 9/18/18
-
+module load R/4.2
 library(data.table)
 library(qqman)
 
@@ -12,6 +12,52 @@ library(qqman)
 # P-value
 #
 # OUTPUT: OUTFILE (string), the name of the pdf to output
+
+# ------------------------------ get.GIF ------------------------------ #
+get.GIF <- function( p.vec )
+# compute genomic inflation factor
+# input: p.vec (numeric), vector of p-values
+# output: GIF
+{
+  median( qchisq(1- p.vec,1))/qchisq(0.5,1)
+}
+# --------------------------------------------------------------------- #
+
+
+# -------------------------- gg.qq ------------------------------------ #
+gg.qq <- function( p.vec, x.pos , y.pos  )
+# recreation of qq from qqman, but using ggplot
+# input:
+#   p.vec (numeric), vector of p-values
+#.  x.pos, y.pos (numeric), x/y coordinates for the position of the GIF text
+# output:
+#   p1, a ggplot object
+{
+
+  if( (!is.numeric(p.vec)))
+      stop("input must be numeric")
+  
+  p.vec <- p.vec[!is.na(p.vec) & !is.nan(p.vec) & !is.null(p.vec) & is.finite(p.vec) &
+                 p.vec < 1 & p.vec > 0]
+  o = -log10(sort(p.vec, decreasing = FALSE))
+  e = -log10(ppoints(length(p.vec)))
+  
+  
+  txt = paste("lambda", round(get.GIF(p.vec),2), sep = " == ")
+  p1 <- ggplot(data = data.frame(o = o, e = e), aes(x = e, y = o)) + geom_point() +
+    theme_minimal() +
+    xlab("Expected") +
+    ylab("Observed") + 
+    geom_abline(slope=1, intercept=0, color="red") +
+     annotate("text", x = x.pos, y = y.pos, 
+              label = txt, 
+              parse = T,
+              size = unit(10, "pt"))
+  return(p1)
+    
+}
+# --------------------------------------------------------------------- #
+
 
 args = commandArgs(trailingOnly=TRUE)
 if( length(args) < 2 )
@@ -34,7 +80,7 @@ k.thresh = 0.2
 
 
 MANOUTFILE <- paste(OUTFILE, "_man.png", sep="")
-
+QQOUTFILE <- paste(OUTFILE, "_qq.png", sep = "")
 # ---
 
 print("reading association testing data...")
@@ -94,6 +140,13 @@ manhattan(dat[(dat$rsid %in% dat$rsid[match(s, dat$rsid)]) | (dat$p <= p.thresh)
         logp=TRUE, chrlabs = chrlabs)
 dev.off()
 
+print("done!")
+
+print("creating qq plot...")
+
+png(QQOUTFILE)
+print(gg.qq(dat$p,1,3))
+dev.off()
 print("done!")
 
 
